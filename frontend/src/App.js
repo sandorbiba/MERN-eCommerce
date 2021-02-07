b29e19190f31bbfe6d2f5afe1414dbb6a5c76ca3
Sandor Biba <sandorbiba@gmail.com>
2021-02-07 17:46:11 +0100

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+// import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcome To ProShop</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
