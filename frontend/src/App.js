95cf188d224c5043cf737b87f6499753e0e20be4
Sandor Biba <sandorbiba@gmail.com>
2021-02-14 11:50:09 +0100

    feat(Products): add Product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 // import "./App.css";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <>
       <Header />
-      <main>
-        <h1>Welcome To ProShop</h1>
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
       </main>
       <Footer />
     </>
