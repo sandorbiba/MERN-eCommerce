6be42a1b9e6f94cb11e02f1a0de8835d7cabb2fd
Sandor Biba <sandorbiba@gmail.com>
2021-02-14 17:15:57 +0100

    feat(Products): add ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,21 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
