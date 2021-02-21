dc0a05b4f3de1614973145a652547486b00a6edd
Sandor Biba <sandorbiba@gmail.com>
2021-02-21 12:13:17 +0100

    feat(backend): add MongoDB & mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -16,4 +22,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const port = process.env.PORT || 5000;
+
+app.listen(
+  port,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {
   Row,
@@ -9,11 +9,19 @@ import {
   Button,
   ListGroupItem,
 } from "react-bootstrap";
-import products from "../products";
 import Rating from "../components/Rating";
+import axios from "axios";
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((prod) => prod._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [match]);
 
   return (
     <>
